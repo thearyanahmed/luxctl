3c63b0601e09e8c0af0eda5151cb88ea8c4991d6
Aryan <paryan@digitalocean.com>
2026-01-20 21:43:06 +0000

    Update API client to use /labs endpoints

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -3,7 +3,7 @@ mod types;
 
 pub use client::{Env, LighthouseAPIClient};
 pub use types::{
-    ApiUser, AttemptData, Hint, PaginatedResponse, PaginationLinks, PaginationMeta, Project,
-    ProjectStats, SubmitAnswerRequest, SubmitAnswerResponse, SubmitAttemptRequest,
-    SubmitAttemptResponse, Task, TaskInputType, TaskOutcome, TaskStatus,
+    ApiUser, AttemptData, Hint, Lab, LabStats, PaginatedResponse, PaginationLinks, PaginationMeta,
+    SubmitAnswerRequest, SubmitAnswerResponse, SubmitAttemptRequest, SubmitAttemptResponse, Task,
+    TaskInputType, TaskOutcome, TaskStatus,
 };
